1ac24603762605edb5b7e71c39536f58d6935ccd
Danilo Barion Nogueira <danilo.barion@vagas.com.br>
2018-12-22 17:59:15 -0200

    Implements .define_action and #all on AsteriskHunter module

--- lib/sequel/extensions/asterisk_hunter.rb
+++ lib/sequel/extensions/asterisk_hunter.rb
@@ -5,8 +5,22 @@ require "sequel"
 module Sequel
   module Extensions
     module AsteriskHunter
-      def hunt(action: :log)
-        return 'Find it!' if self.inspect.include?('SELECT *')
+      @@action = -> { puts "Find 'SELECT *' in query!" }
+
+      def all
+        hunt
+        super
+      end
+
+      def self.define_action(action)
+        raise TypeError, 'Action parameter must be a callable object!' unless action.respond_to?(:call)
+        @@action = action
+      end
+
+      private
+
+      def hunt
+        @@action.call if self.inspect.include?('SELECT *')
         self
       end
     end
